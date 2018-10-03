4411bed19c842407bc26a922bf92ede5230fc1e5
Hafiz Adewuyi <hoadewuyi@gmail.com>
2018-10-03 15:16:01 +0100

    Refactor Yoruba Tone Marker service

--- FMTest/js/app.js
+++ FMTest/js/app.js
@@ -13,32 +13,6 @@
             }
         };
     }])
-    .factory('YorubaIntonationService', function ($http) {
-        var service = {
-            getGreeting: getGreeting,
-            processParagraph: processParagraph,
-            notifyCompleted: notifyCompleted,
-            getRecentlyProcessed: getRecentlyProcessed
-        };
-
-        function getGreeting() {
-            return "Hello world";
-        }
-
-        function processParagraph(word) {
-            return $http.post('api/yoruba/paragraph/derivatives', { content: word });
-        }
-
-        function notifyCompleted(id, finalForm) {
-            return $http.put('api/yoruba/paragraph/derivatives/?noblockui', { id: id, output: finalForm });
-        }
-
-        function getRecentlyProcessed() {
-            return $http.get('/api/yoruba/paragraph/derivatives');
-        }
-
-        return service;
-    })
     .controller('JsEditorController', function ($scope, JsEditorService, $window) {
         $scope.processArray = processArray;
         $scope.columnChanged = columnChanged;
