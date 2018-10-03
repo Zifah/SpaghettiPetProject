45f8d376836272827299610024bd7356361aea52
Hafiz Adewuyi <hoadewuyi@gmail.com>
2018-10-03 15:38:26 +0100

    Rename business logic folder from System to Business

---
+++ FMTest/Business/YorubaFeedbackService.cs
@@ -0,0 +1,16 @@
+﻿using FMTest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FMTest.Business
+{
+    public class YorubaFeedbackService
+    {
+        public static void SubmitFeedback(Feedback feedback)
+        {
+
+        }
+    }
+}
